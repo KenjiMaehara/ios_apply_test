357953b21a3b73ee90ca234b5e5f278af2cfe4bb
KenjiMaehara <kmaehara@mel.co.jp>
2016-05-23 20:28:29 +0900

    iOS project add

---
+++ proxyView20160510/proxyView20160510/ViewController.swift
@@ -0,0 +1,101 @@
+//
+//  ViewController.swift
+//  proxyView20160510
+//
+//Users/maeharakenji/Desktop/img_iccard.png/  Created by 前原健二 on 2016/05/10.
+//  Copyright © 2016年 前原健二. All rights reserved.
+//
+
+import UIKit
+//import Cocoa
+
+class ViewController: UIViewController {
+
+    //let dateLabel = UILabel()  // 日時表示ラベル
+    
+    
+    
+    @IBOutlet weak var tokeiLabel: UILabel!
+    
+    
+    // 日時フォーマット
+    var dateFormatter: NSDateFormatter{
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
+        return formatter
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+        /*
+        // 日時をラベルに表示する
+        var ima = self.getDate()
+        self.tokeiLabel.text = "日時： \(ima)"
+        self.tokeiLabel.textColor = UIColor.purpleColor()
+        */
+        
+        
+        
+        //dateLabel.frame = view.bounds
+        //dateLabel.textAlignment = .Center
+        //view.addSubview(dateLabel)
+        //view.addSubview(tokeiLabel)
+        
+        // 初回
+        updateDateLabel()
+        
+        
+        // 一定間隔で実行
+        NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "updateDateLabel", userInfo: nil, repeats: true)
+        
+    
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    
+    
+    var j=0
+    
+    // 日時表示ラベル更新メソッド
+    func updateDateLabel(){
+        
+        //j++
+        
+        
+        
+        let now = NSDate()
+        //dateLabel.text = dateFormatter.stringFromDate(now)
+        tokeiLabel.text = dateFormatter.stringFromDate(now)
+        
+        //self.tokeiLabel.text = dateFormatter.stringFromDate(now)
+ 
+        
+ 
+    }
+ 
+    
+    
+    /*
+    // 日時を取得する
+    func getDate()->NSString {
+        let now = NSDate()
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.locale = NSLocale(localeIdentifier: "en_US") // ロケールの設定
+        dateFormatter.dateFormat = "yyyy/MM/dd HH:mm:ss" // 日付フォーマットの設定
+        var thisDate = dateFormatter.stringFromDate(now)
+        return thisDate
+    }
+    */
+ 
+
+}
+
---
+++ proxyView20160510/proxyView20160510Tests/proxyView20160510Tests.swift
@@ -0,0 +1,36 @@
+//
+//  proxyView20160510Tests.swift
+//  proxyView20160510Tests
+//
+//  Created by 前原健二 on 2016/05/10.
+//  Copyright © 2016年 前原健二. All rights reserved.
+//
+
+import XCTest
+@testable import proxyView20160510
+
+class proxyView20160510Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
