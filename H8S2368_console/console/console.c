9c01868166d9fcfab4531ce02e0479ff30de04b6
blackzoro <diy_sunny@yahoo.co.jp>
2016-06-27 11:28:40 +0900

    sci0のprintfの追加
    SCI0ポートで使用するprintfを追加したコード修正。

--- H8S2368_console/console/console.c
+++ H8S2368_console/console/console.c
@@ -1,4 +1,6 @@
 #include "all_includes.h"
+//#include <stdarg.h>
+
 
 /** ************************************************************************* ** 
  ** defines
@@ -13,11 +15,23 @@
  ** locals
  ** ************************************************************************* **/
 
-
+//#include <stdarg.h>
 
 char charget(void);
 void charput(char);
 void PutStr(char *);
+
+
+void charput_sci1(char OutputChar);
+void outs_sci1(char *, short);
+void outc_sci1(char, short);
+void outn_sci1(char, unsigned long, short);
+void mprint_test_sci1(char *fmt, ...);
+
+
+
+
+
 
 #ifdef CONSOLE_DEBUG
 char charget(void)
@@ -71,5 +85,127 @@ void charput(char OutputChar)
 void PutStr(char *str)
 {
 }
+
+
+
+
+
+ 
+/*------------------------------------------------------------------------------
+ +　書式付きターミナル出力
+ +----------------------------------------------------------------------------*/
+
+
+void mprint_test_sci1(char *fmt, ...)
+{
+
+#if 1
+
+    va_list args;
+    char *p;
+    unsigned long v;
+    short c;
+ 
+
+
+    va_start(args, fmt);
+    for (p = fmt; *p != '\0'; p++) {
+        if (*p == '%') {
+            p++;
+            if ((*p >= '1') && (*p <= '9')) {
+                c = *p++ - '0';
+                while ((*p >= '0') && (*p <= '9')) c = c * 10 + (*p++ - '0');
+            }
+            else
+                c = -1;
+ 
+            v = (unsigned long)va_arg(args, long);
+ 
+            switch (*p) {
+            case 's':
+                outs_sci1((char*)v, c);
+                break;
+            case 'c':
+                outc_sci1((char)v, c);
+                break;
+            case 'd': case 'o': case 'x':
+                outn_sci1(*p, v, c);
+            }
+            if (*p == '\0') goto fin;
+        }
+        else if (*p == '\n') {
+#ifdef H8TINY_USB
+            charput_sci1('\r');
+#endif
+            charput_sci1('\n');
+        }
+        else
+            charput_sci1(*p);
+    }
+fin:
+    va_end(args);
+
+#endif
+
+}
+ 
+/*------------------------------------------------------------------------------
+ +　文字列を出力する
+ +----------------------------------------------------------------------------*/
+static void outs_sci1(char *s, short c)
+{
+    char *p;
+    for (p = s; *p != '\0'; p++) {
+        if (c-- == 0) return;
+        //output(*p);
+        charput_sci1(*p)
+    }
+    while (c-- > 0) charput_sci1(' ');
+}
+ 
+/*------------------------------------------------------------------------------
+ +　文字を出力する
+ +----------------------------------------------------------------------------*/
+static void outc_sci1(char ch, short c)
+{
+    charput_sci1(ch);
+    while (--c > 0) charput_sci1(' ');
+}
+ 
+/*------------------------------------------------------------------------------
+ +　整数を出力する
+ +----------------------------------------------------------------------------*/
+static void outn_sci1(char fm, unsigned long v, short c)
+{
+    char s[C_MAX], *p, z;
+    unsigned short b;
+ 
+    switch (fm) {
+    case 'd':
+        b = 10; z = ' '; break;
+    case 'o':
+        b = 8; z = '0'; break;
+    case 'x':
+        b = 16; z = '0'; break;
+    default:
+        return;
+    }
+    p = s;
+    do {
+        c--;
+        *p++ = "0123456789abcdef"[v % b];
+        v /= b;
+    } while (v != 0);
+ 
+    for (; c > 0; c--) charput_sci1(z);
+ 
+    while (p > s) charput_sci1(*--p);
+} 
+
+
+
+
+
+
 
 #endif
